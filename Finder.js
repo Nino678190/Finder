cbb7e7a161e840dfe15c8ae32258ec338a164fe2
Ben Ben <Behujan@outlook.de>
2023-09-16 21:04:27 +0200

    Has a Match page mit einer Animation

---
+++ Finder.js
@@ -0,0 +1,25 @@
+/*const user = JSON.parse(userJSON)
+
+const name = user.name 
+    
+const pronomen = user.pronomen
+
+const geburtsdatum = user.geburtsdatum
+
+const wohnort = user.wohnort
+
+const hobbies = user.hobbies
+
+const beschreibung = user.hobbies*/
+
+
+window.addEventListener("load",function(){
+    const el=document.getElementById("Animation");
+    const el2=document.querySelector(".spacer"); 
+
+    window.setTimeout(() => {
+        el.style.display="none";
+        el2.style.display="none";
+    }, 8000);
+})
+
