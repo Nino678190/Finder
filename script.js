899f4b73ed421d2d4ab4397775186de3f3441b35
Nino678190 <superwauzi@gmx.de>
2023-09-16 20:56:28 +0200

    Versuch Datenbank einzubinden

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function ansValidation() {
     }else if (passValue !== confpassValue) {
         window.alert("Passwort stimmt nicht überein")
     }
+
+var db = openDatabase('mydb', '1.0', 'my first database', 2 * 1024 * 1024);
+db.transaction(function (tx, gametag, email, geburtsdatum, pronomen, spiele, alpakaavatar, password) {
+    tx.executeSql('CREATE TABLE IF NOT EXISTS USER(' +
+    '"id" INTEGER NOT NULL UNIQUE PRIMARY KEY ,' +
+    '"gametag" TEXT NOT NULL,' +
+    '"pronomen" TEXT,' +
+    '"geburtsdatum" Text NOT NULL,' +
+    '"spiele" Text NOT NULL,' +
+    '"avatar" INTEGER, ' +
+    '"email" text NOT NULL,' +
+    '"password" text NOT NULL)')
+    });
