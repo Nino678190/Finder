70f7ed591a430b9f7d6f0b3f18aef5baeecaf081
Nino678190 <superwauzi@gmx.de>
2023-09-17 12:48:47 +0200

    DB einbinden

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function ansValidation() {
     var passValue = document.getElementById("password").value
     var confpassValue = document.getElementById("confirmPassword").value
     if (passValue === confpassValue) {
+        window.alert("Das hat geklappt")
         return true;
     } else {
         window.alert("Passwort stimmt nicht überein");
